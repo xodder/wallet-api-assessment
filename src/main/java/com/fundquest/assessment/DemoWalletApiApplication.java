13acc5b9b288184364e42e4953e8005fbcf64cbe
Stephen Odebiyi <odebiyistephen@gmail.com>
2023-04-12 22:53:13 +0100

    Initial commit

---
+++ src/main/java/com/fundquest/assessment/DemoWalletApiApplication.java
@@ -0,0 +1,13 @@
+package com.assessment.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWalletApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWalletApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assessment/wallet/DemoWalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWalletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
