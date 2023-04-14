189543b1876753315290766297e4011fc8975624
Stephen Odebiyi <odebiyistephen@gmail.com>
2023-04-14 06:27:58 +0100

    Fix Pageable and Sort not allowed issue

--- src/main/java/com/fundquest/assessment/transaction/TransactionRepository.java
+++ src/main/java/com/fundquest/assessment/transaction/TransactionRepository.java
@@ -2,12 +2,11 @@ package com.fundquest.assessment.transaction;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-    Page<Transaction> findByUserId(Long userId, Pageable pageable, Sort sort);
+    Page<Transaction> findByUserId(Long userId, Pageable pageable);
 
     Page<Transaction> findByUserIdOrderByCreatedAtDesc(Long userId, Pageable pageable);
 
--- src/main/java/com/fundquest/assessment/wallet/deps/history/WalletBalanceHistoryRepository.java
+++ src/main/java/com/fundquest/assessment/wallet/deps/history/WalletBalanceHistoryRepository.java
@@ -2,12 +2,11 @@ package com.fundquest.assessment.wallet.deps.history;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WalletBalanceHistoryRepository extends JpaRepository<WalletBalanceHistory, Long> {
 
-    Page<WalletBalanceHistory> findByWalletId(Long walletId, Pageable pageable, Sort sort);
+    Page<WalletBalanceHistory> findByWalletId(Long walletId, Pageable pageable);
 
     Page<WalletBalanceHistory> findByWalletIdOrderByRecordedAtDesc(Long walletId, Pageable pageable);
 
