07c27ade8a89e2264e5eaabc8e85a453746d179d
Stephen Odebiyi <odebiyistephen@gmail.com>
2023-04-14 06:27:18 +0100

    Fix wrong package name

--- src/main/java/com/fundquest/assessment/DemoWalletApiApplication.java
+++ src/main/java/com/fundquest/assessment/DemoWalletApiApplication.java
@@ -1,4 +1,4 @@
-package com.assessment.wallet;
+package com.fundquest.assessment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
